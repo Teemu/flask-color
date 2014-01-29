7940a609dc77654206d3b26b15ab1f40302d5994
Teemu <teemu.on.ihminen@gmail.com>
2014-01-29 20:46:44 +0200

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Color',
-    version='0.1',
+    version='0.2',
     url='https://github.com/Frozenball/flask-color',
     license='MIT',
     author='Frozenball',
